893dcc8913e03ea2f688407180c7d82cb406a79e
Blake Friedman <blakef@meta.com>
2024-06-21 13:57:18 +0100

    refactor: automation to tag and version the template when releasing (#30)
    There was a discussion on Discord, where we looked at implementing what was originally proposed.  This is that implementation.  It'll need to land with a [cli](https://github.com/react-native-community/cli/pull/2422) change to use the update template + tagging.
    
    - feat: add mutating react-native & @react-native/ packages
    
        Support for doing this on nightly and release workflows
    
    - feat: added node ./scripts/updateReactNativeVersion.js <version>
    
        Sets the version of react-native and the @react-native/ scoped packages.
        It will also normalize tags to concrete versions. E.g. latest -> 0.74.2
    
    - refactor: move scripts into a ./scripts folder

---
+++ scripts/lib/lib.js
@@ -0,0 +1,25 @@
+const {execSync} = require('child_process');
+
+const registry = getNpmRegistryUrl();
+
+function getNpmRegistryUrl() {
+  try {
+    return execSync("npm config get registry").toString().trim();
+  } catch {
+    return "https://registry.npmjs.org/";
+  }
+}
+
+/**
+ * Convert an npm tag to a concrete version, for example:
+ * - next -> 0.75.0-rc.0
+ * - nightly -> 0.75.0-nightly-20240618-5df5ed1a8
+ */
+async function npmMaterializeVersion(packageName, tagOrVersion) {
+  const url = new URL(registry);
+  url.pathname = `${packageName}/${tagOrVersion}`;
+  const json = await fetch(url).then((resp) => resp.json());
+  return json.version;
+}
+
+module.exports.npmMaterializeVersion = npmMaterializeVersion;
