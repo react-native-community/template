10925e08ee4a302f49bfc14ecbc0d7af2e0304e8
Alex Hunt <hello@alexhunt.io>
2024-09-27 14:23:16 +0100

    Add @types/jest and simplify test example

--- template/__tests__/App.test.tsx
+++ template/__tests__/App.test.tsx
@@ -2,17 +2,11 @@
  * @format
  */
 
-import 'react-native';
 import React from 'react';
+import ReactTestRenderer from 'react-test-renderer';
 import App from '../App';
 
-// Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
-
-// Note: test renderer must be required after react-native.
-import ReactTestRenderer from 'react-test-renderer';
-
-it('renders correctly', async () => {
+test('renders correctly', async () => {
   await ReactTestRenderer.act(() => {
     ReactTestRenderer.create(<App />);
   });
