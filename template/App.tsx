7a2432441c1aedd7ea707a92c14ed2b9ecef475c
Alex Hunt <hello@alexhunt.io>
2025-05-01 16:55:49 +0100

    Reset Prettier brackets options to defaults (#125)

--- template/App.tsx
+++ template/App.tsx
@@ -5,22 +5,15 @@
  * @format
  */
 
-import {NewAppScreen} from '@react-native/new-app-screen';
-import {
-  StatusBar,
-  StyleSheet,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { NewAppScreen } from '@react-native/new-app-screen';
+import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <View style={styles.container}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-      />
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <NewAppScreen templateFileName="App.tsx" />
     </View>
   );
