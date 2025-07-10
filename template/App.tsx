de2aaa49ae4b83558dffe35092e385f3ea877184
Alex Hunt <hello@alexhunt.dev>
2025-07-10 12:56:40 +0100

    [hotfix] Temporarily remove dep on react-native-safe-area-context (#153)

--- template/App.tsx
+++ template/App.tsx
@@ -7,31 +7,23 @@
 
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaProvider>
+    <>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <AppContent />
-    </SafeAreaProvider>
+    </>
   );
 }
 
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
+  // TODO(huntie): Restore safeAreaInsets assignment (see #153)
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <NewAppScreen templateFileName="App.tsx" />
     </View>
   );
 }
