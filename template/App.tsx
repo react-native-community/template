a808e9f41acc8206a3a727559259939a31ecd8cd
Alex Hunt <hello@alexhunt.dev>
2025-07-22 13:39:12 +0100

    Restore SafeAreaProvider in App.tsx
    This reverts commit de2aaa49ae4b83558dffe35092e385f3ea877184.

--- template/App.tsx
+++ template/App.tsx
@@ -7,23 +7,31 @@
 
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <>
+    <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <AppContent />
-    </>
+    </SafeAreaProvider>
   );
 }
 
 function AppContent() {
-  // TODO(huntie): Restore safeAreaInsets assignment (see #153)
+  const safeAreaInsets = useSafeAreaInsets();
+
   return (
     <View style={styles.container}>
-      <NewAppScreen templateFileName="App.tsx" />
+      <NewAppScreen
+        templateFileName="App.tsx"
+        safeAreaInsets={safeAreaInsets}
+      />
     </View>
   );
 }
