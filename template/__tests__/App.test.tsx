8efae899d25e421abcc12713d902c6bab55a84b3
matinzd <24797481+matinzd@users.noreply.github.com>
2025-09-13 10:17:29 +0200

    fix: remove async

--- template/__tests__/App.test.tsx
+++ template/__tests__/App.test.tsx
@@ -5,6 +5,6 @@
 import { render } from '@testing-library/react-native';
 import App from '../App';
 
-test('renders correctly', async () => {
+test('renders correctly', () => {
   render(<App />);
 });
