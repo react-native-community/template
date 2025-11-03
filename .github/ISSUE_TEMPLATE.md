09414198e148836439066371e1d60ab3c67da2ab
Alex Hunt <hello@alexhunt.dev>
2025-11-03 17:57:21 +0000

    Add Prettier, format files

--- .github/ISSUE_TEMPLATE.md
+++ .github/ISSUE_TEMPLATE.md
@@ -1,4 +1,3 @@
 ✋ To keep the backlog clean and actionable, issues will be
 🚫 closed if they do not follow one of the issue templates:
 👉 https://github.com/react-native-community/template/issues/new/choose
-
--- scripts/__tests__/bumpTemplateVersion-test.js
+++ scripts/__tests__/bumpTemplateVersion-test.js
@@ -1,14 +1,19 @@
-const {execSync, exec: _exec} = require('child_process');
+const { execSync, exec: _exec } = require('child_process');
 const fs = require('fs');
-const {promisify} = require('util');
+const { promisify } = require('util');
 const path = require('path');
 const semver = require('semver');
 
 function run(version) {
-  return execSync(`./bumpedTemplateVersion.sh ${version}`, { cwd: 'scripts', stdio: ['ignore', 'pipe', 'ignore'] }).toString().trim();
+  return execSync(`./bumpedTemplateVersion.sh ${version}`, {
+    cwd: 'scripts',
+    stdio: ['ignore', 'pipe', 'ignore'],
+  })
+    .toString()
+    .trim();
 }
 
-const FIFO_PATH = '/tmp/_npm_fifo'
+const FIFO_PATH = '/tmp/_npm_fifo';
 
 const exec = promisify(_exec);
 const writeFile = promisify(fs.writeFile);
@@ -16,19 +21,17 @@ const writeFile = promisify(fs.writeFile);
 async function runStubbedNpm(version, response) {
   cleanUpStubbedNpm();
   execSync(`mkfifo ${FIFO_PATH}`);
-  return Promise.all(
-    [
-      writeFile(FIFO_PATH, JSON.stringify(response)),
-      exec(`./bumpedTemplateVersion.sh ${version}`, {
-        cwd: 'scripts',
-        stdio: ['ignore', 'pipe', 'ignore'],
-        env: { 
-          PATH: `${__dirname}/stub:${process.env.PATH}`,
-          NPM_STUB_FIFO: FIFO_PATH,
-        }
-      }).then(raw => raw.stdout.toString().trim()),
-    ]
-  ).then(([, resp]) => resp);
+  return Promise.all([
+    writeFile(FIFO_PATH, JSON.stringify(response)),
+    exec(`./bumpedTemplateVersion.sh ${version}`, {
+      cwd: 'scripts',
+      stdio: ['ignore', 'pipe', 'ignore'],
+      env: {
+        PATH: `${__dirname}/stub:${process.env.PATH}`,
+        NPM_STUB_FIFO: FIFO_PATH,
+      },
+    }).then(raw => raw.stdout.toString().trim()),
+  ]).then(([, resp]) => resp);
 }
 
 function cleanUpStubbedNpm() {
@@ -40,9 +43,10 @@ function cleanUpStubbedNpm() {
 }
 
 describe('bumpTemplateVersion.sh', () => {
-
   it('nightlies stay the same', () => {
-    expect(run('0.75.0-nightly-20240606-4324f0874')).toEqual('0.75.0-nightly-20240606-4324f0874');
+    expect(run('0.75.0-nightly-20240606-4324f0874')).toEqual(
+      '0.75.0-nightly-20240606-4324f0874',
+    );
   });
 
   it('release candidates appended with a sha', () => {
@@ -56,24 +60,32 @@ describe('bumpTemplateVersion.sh', () => {
 
   it('bumps the patch if a version of the template already exists', async () => {
     // This sucks I know:
-    const {version} = await fetch('https://registry.npmjs.com/@react-native-community/template/latest').then(resp => resp.json());
-    const {major, minor, patch} = semver.parse(version);
-    expect(run(`${major}.${minor}.${patch}`)).toEqual(`${major}.${minor}.${patch+1}`);
+    const { version } = await fetch(
+      'https://registry.npmjs.com/@react-native-community/template/latest',
+    ).then(resp => resp.json());
+    const { major, minor, patch } = semver.parse(version);
+    expect(run(`${major}.${minor}.${patch}`)).toEqual(
+      `${major}.${minor}.${patch + 1}`,
+    );
   });
 
   describe('handles different npm responses', () => {
     afterAll(cleanUpStubbedNpm);
     it('arrays of versions', async () => {
-      expect(await runStubbedNpm('0.76.0', [
-        { version: '0.76.0' },
-        { version: '0.76.2' },
-        // Expecting +1 on this:
-        { version: '0.76.3' },
-      ])).toEqual('0.76.4');
+      expect(
+        await runStubbedNpm('0.76.0', [
+          { version: '0.76.0' },
+          { version: '0.76.2' },
+          // Expecting +1 on this:
+          { version: '0.76.3' },
+        ]),
+      ).toEqual('0.76.4');
     });
 
     it('single version', async () => {
-      expect(await runStubbedNpm('0.76.0', { version: '0.76.0' })).toEqual('0.76.1');
+      expect(await runStubbedNpm('0.76.0', { version: '0.76.0' })).toEqual(
+        '0.76.1',
+      );
     });
   });
 });
